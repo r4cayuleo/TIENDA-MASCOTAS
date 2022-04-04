a7f7a57d747d78cc78b73b38ec7fa9e3f1070b99
r4cayuleo <ra.cayuleo@duocuc.cl>
2022-04-03 20:23:03 -0400

    cuarto commit

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+ScrollReveal().reveal('.noticiaprincipal', {delay: 300});
+ScrollReveal().reveal('.res-destacada', {delay: 300});
+ScrollReveal().reveal('.cards-banner-one', {delay: 300});
+ScrollReveal().reveal('.cards-banner-two', {delay: 300});
