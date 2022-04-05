069db529273afc6858a877440fa5099e1dc229a0
r4cayuleo <ra.cayuleo@duocuc.cl>
2022-04-04 20:30:18 -0400

    septimo commit

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ ScrollReveal().reveal('.noticiaprincipal', {delay: 300});
 ScrollReveal().reveal('.res-destacada', {delay: 300});
 ScrollReveal().reveal('.cards-banner-one', {delay: 300});
 ScrollReveal().reveal('.cards-banner-two', {delay: 300});
+
+// INDEX LIBROS
+ScrollReveal().reveal('', {delay: 300});
+
+// INDEX RESEÑAS
+ScrollReveal().reveal('', {delay: 300});
+
+// INDEX AUTORES
+ScrollReveal().reveal('', {delay: 300});
+
+// INDEX ULTIMA PUBLICACION
+ScrollReveal().reveal('', {delay: 300});
