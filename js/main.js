1ac34d1df84e08ce999cb9f9c063bf8b0d3ebab0
r4cayuleo <ra.cayuleo@duocuc.cl>
2022-04-12 16:45:47 -0400

    commit 14

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.principal', {delay: 300});
+ScrollReveal().reveal('.titulos-accmasc', {delay: 300});
+ScrollReveal().reveal('.accesorios-masc', {delay: 300});
+
+
+// // INDEX ALIMENTOS
+// ScrollReveal().reveal('', {delay: 300});
+
+// // INDEX ACCESORIOS
+// ScrollReveal().reveal('', {delay: 300});
+
+// // INDEX SALUD Y CUIDADO
+// ScrollReveal().reveal('', {delay: 300});
