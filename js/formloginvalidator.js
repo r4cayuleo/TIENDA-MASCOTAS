60ad1edcf3144c836efb97c61a71e7bd33a84899
r4cayuleo <ra.cayuleo@duocuc.cl>
2022-05-12 02:05:06 -0400

    commit 19

--- js/formloginvalidator.js
+++ js/formloginvalidator.js
@@ -1,43 +1,31 @@
 // put validations using jquery
-$(document).ready(function(){
+$(document).ready(function() {
     $("#email").keyup(function(){
-        var regx_email=/^([a-zA-Z]+) ([0-9]+)? (@) ([a-ZA-Z]{5,10}(.)([a-ZA-Z]+))$/i;
+        var regx_email = /^([a-zA-Z]+) ([0-9]+)? (@) ([a-ZA-Z]{5,10}(.)([a-zA-Z]+))$/i;
 
-        var email_inp=$(this).val();
-    if (regx_email.test(email_inp)){
-        $("#email_status").text("valid");
-        $("#email status").removeClass("text-danger");
-    }
-    else{
-        $("#email_status").addClass("text-danger");
-        $("#email_status").text ("Invalid");
-    }
+        var email_inp = $(this).val();
+        if (regx_email.test(email_inp)) {
+            $("#email_status").text("valid");
+            $("#email status").removeClass("text-danger"); 
+        }
+        else {
+            $("#email_status").addClass("text-danger");
+            $("#email_status").text ("Invalid");
+        }
 });
 
-$("#pass").keyup(function(){
-    var regx_pass=/^([a-ZA-Z]+)([0-9]+)([$&+,:;=?@#|'<>.**()%!-]+)$/i;
-    var pass=$(this).val();
-    if (regx_pass.test(pass)){
-        $("#pass_status").text("valid");
-        $("#pass_status").removeClass("text-danger");
-    }
-    else {
-        $("#pass_status").text("Invalid");
-        $("#pass_status").addClass("text-danger");
-    }
-});
+    $("#pass").keyup(function(){
+        var regx_pass = /^([a-zA-Z]+)([0-9]+)([$&+,:;=?@#|'<>.^*()%!-]+)$/i;
 
-$("#pass").keyup(function(){
-    var regx_pass = /^([a-zA-Z]+)([0-9]+)([$&+,:;=?@#|'<>,^*()%!-]+)$/i;
-    var pass=$(this).val();
-    if (regx_pass.test(pass)){
-        $("#pass_status").text ("valid");
-        $("#pass_status").removeClass("text-danger");
-    }
-    else{
-        $("#pass_status").addClass("text-danger");
-        $("#pass_status").text("Invalid");
-    }
-    });
+        var pass = $(this).val();
+        if (regx_pass.test(pass)){
+            $("#pass_status").text("valid");
+            $("#pass_status").removeClass("text-danger");
+        }
+        else {
+            $("#pass_status").text("Invalid");
+            $("#pass_status").addClass("text-danger");
+        }
+        });
 
 });
